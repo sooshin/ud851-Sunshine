7aed6ff522a6c3fcec2252053c79c4b24374f762
Soojeong <soojeongshin2@gmail.com>
2018-05-01 20:49:35 +0900

    S10.02-Compare with the solution

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -18,6 +18,7 @@ package com.example.android.sunshine.sync;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 
@@ -30,35 +31,56 @@ public class SunshineSyncUtils {
     private static boolean sInitialized;
 
     //  TODO (2) Create a synchronized public static void method called initialize
-    synchronized public static void initialize(final Context context) {
+    /**
+     * Creates periodic sync tasks and checks to see if an immediate sync is required. If an
+     * immediate sync is required, this method will take care of making sure that sync occurs.
+     *
+     * @param context Context that will be passed to other methods and used to access the
+     *                ContentResolver
+     */
+    synchronized public static void initialize(@NonNull final Context context) {
 
     //  TODO (3) Only execute this method body if sInitialized is false
-        if (!sInitialized) {
+        if (sInitialized) return;
     //  TODO (4) If the method body is executed, set sInitialized to true
-            sInitialized = true;
+        sInitialized = true;
 
     //  TODO (5) Check to see if our weather ContentProvider is empty
-            new AsyncTask<Void, Void, Void>() {
+        new AsyncTask<Void, Void, Void>() {
 
-                @Override
-                protected Void doInBackground(Void... voids) {
-                    Cursor cursor = context.getContentResolver().query(
-                            WeatherContract.WeatherEntry.CONTENT_URI,
-                            null,
-                            null,
-                            null,
-                            null
-                    );
+            @Override
+            protected Void doInBackground(Void... voids) {
+
+                Uri forecastQueryUri = WeatherContract.WeatherEntry.CONTENT_URI;
+
+                /*
+                 * Since this query is going to be used only as a check to see if we have any
+                 * data (rather than to display data), we just need to PROJECT the ID of each
+                 * row. In our queries where we display data, we need to PROJECT more columns
+                 * to determine what weather details need to be displayed.
+                 */
+                String[] projectionColumns = {WeatherContract.WeatherEntry._ID};
+                String selectionStatement = WeatherContract.WeatherEntry
+                        .getSqlSelectForTodayOnwards();
+
+                Cursor cursor = context.getContentResolver().query(
+                       forecastQueryUri,
+                        projectionColumns,
+                        selectionStatement,
+                        null,
+                        null
+                );
         //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
-                    if (cursor.getCount() == 0 || cursor == null) {
-                        startImmediateSync(context);
-                    }
-                    cursor.close();
-                    return null;
+                if (cursor.getCount() == 0 || null == cursor) {
+                    startImmediateSync(context);
                 }
-            }.execute();
-        }
+                /* Make sure to close the Cursor to avoid memory leaks! */
+                cursor.close();
+                return null;
+            }
+        }.execute();
     }
+
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
      * execution.
